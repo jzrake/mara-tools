7b7b0776301f02f30ac4a6e66b153e80a1350f70
Jonathan Zrake <jonathan.zrake@gmail.com>
2014-02-27 15:50:55 -0800

    Add a line integral convolution sub-module

--- mara_tools/cutplanes.py
+++ mara_tools/cutplanes.py
@@ -25,3 +25,28 @@ class MaraCheckpointCutplaneExtractor(MaraTool):
         plt.setp(sax.get_xticklabels(), visible=False)
         plt.setp(sax.get_yticklabels(), visible=False)
         plt.show()
+
+    @logmethod
+    def plot_lic(self, index=0, texture=None):
+        import matplotlib.pyplot as plt
+        from lic import lic_internal
+
+        v1 = self._chkpt['prim']['By'][index,:,:]
+        v2 = self._chkpt['prim']['Bz'][index,:,:]
+        N = v1.shape
+
+        vectors = np.zeros(N + (2,), dtype=np.float32)
+        vectors[:,:,0] = v1
+        vectors[:,:,1] = v2
+
+        if texture is None:
+            texture = np.random.rand(N[0],N[1]).astype(np.float32)
+
+        kernellen = 32
+        kernel = np.sin(np.arange(kernellen)*np.pi/kernellen).astype(np.float32)
+        image = lic_internal.line_integral_convolution(vectors, texture, kernel)
+
+        plt.imshow(image, cmap='bone', interpolation='nearest')
+        plt.show()
+
+        return texture
